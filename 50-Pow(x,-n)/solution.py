c77d16657663f22af83470dbd28a88352ecc3a52
Eric Yang <Windfarer@users.noreply.github.com>
2016-08-11 18:44:32 +0800

    +1s

--- 50-Pow(x,-n)/solution.py
+++ 50-Pow(x,-n)/solution.py
@@ -6,18 +6,18 @@ class Solution(object):
         :rtype: float
         """
         if x == 0:
-    		return 0
-    	if n < 0:
-    		x = 1 / x
-    		n = -n
-    	result = 1
-    	while n > 0:
-    		if n % 2 == 0:
-    			x = x * x
-    			n = n // 2
-    			continue
-    		else:
-    			result = result * x
-    			n = n - 1
-    	return result
+            return 0
+        if n < 0:
+            x = 1 / x
+            n = -n
+        result = 1
+        while n > 0:
+            if n % 2 == 0:
+                x = x * x
+                n = n // 2
+                continue
+            else:
+                result = result * x
+                n = n - 1
+        return result
         # return pow(x, n)  # just for fun
