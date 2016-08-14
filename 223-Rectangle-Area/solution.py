7fcdbcc059f7ab5143138a0d49c03106cfdebf26
Eric Yang <Windfarer@users.noreply.github.com>
2016-08-14 23:38:53 +0800

    [223. Rectangle Area][Accepted] +1s

--- 223-Rectangle-Area/solution.py
+++ 223-Rectangle-Area/solution.py
@@ -14,19 +14,14 @@ class Solution(object):
         return (C-A)*(D-B)+(G-E)*(H-F)-self.get(A,C,E,G)*self.get(B,D,F,H)
         
     def get(self, lower, upper, x, y):
-        p1 = bool(x<lower)
-        q1 = bool(lower<=x<=upper)
-        r1 = bool(x>upper)
-        p2 = bool(y<lower)
-        q2 = bool(lower<=y<=upper)
-        r2 = bool(y>upper)
-        if p1 and q2:
+        p1 = bool()
+        if x<lower and lower<=y<=upper:
             return y - lower
-        elif p1 and r2:
+        elif x<lower and y>upper:
             return upper - lower
-        elif q1 and q2:
+        elif lower<=x<=upper and lower<=y<=upper:
             return y - x
-        elif q1 and r2:
+        elif lower<=x<=upper and y>upper:
             return upper - x
         return 0
         
