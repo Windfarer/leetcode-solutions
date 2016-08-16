f80af102a6e8523ddc51c3a41afe786dc296685f
Eric Yang <Windfarer@users.noreply.github.com>
2016-08-16 08:05:01 +0800

    [1. Two Sum][Accepted] +1s

--- 1-Two-Sum/solution.py
+++ 1-Two-Sum/solution.py
@@ -5,9 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        d = {}
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        
-            
+            if target - nums[i] in d:
+                return [d[target - nums[i]], i]
+            else:
+                d[nums[i]] = i
+        print d
+    
