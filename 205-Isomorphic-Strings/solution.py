d2fb79bdad8cba653c923c9d9288bfb1c568f0c3
Eric Yang <Windfarer@users.noreply.github.com>
2016-08-17 01:05:06 +0800

    [205. Isomorphic Strings][Accepted] +1s

---
+++ 205-Isomorphic-Strings/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        d = {}
+        for i, j in zip(s,t):
+            if i not in d and j not in d.values():
+                d[i] = j
+            else:
+                if d.get(i) != j:
+                    return False
+        return True
+                
+            
