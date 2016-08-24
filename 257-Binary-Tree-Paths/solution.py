157bfda344811e23be4c6b78f828eaa7cb9eaa4a
Eric Yang <Windfarer@users.noreply.github.com>
2016-08-24 22:38:15 +0800

    [257. Binary Tree Paths][Accepted] +1s

--- 257-Binary-Tree-Paths/solution.py
+++ 257-Binary-Tree-Paths/solution.py
@@ -10,24 +10,18 @@ class Solution:
     # @return {string[]}
     def binaryTreePaths(self, root):
         self.result = []
-        if root:
-            self.walk(root, [])
-        print(self.result)
+        self.walk(root, [])
         return ["->".join(map(str,i)) for i in self.result]
     
     def walk(self, node, collected):
         if node is not None:
             collected = collected + [node.val]
-            if self.isLeaf(node):
-                print collected
+            if node is not None and node.left is None and node.right is None:
                 self.result.append(collected)
                 return 
             if node.left:
                 self.walk(node.left, collected)
             if node.right:
                 self.walk(node.right, collected)
-    
-    def isLeaf(self, node):
-        return node is not None and node.left is None and node.right is None
 
     
