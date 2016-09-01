1bdef20988eaf300f431636ffb2b40762b4d090b
Eric Yang <Windfarer@users.noreply.github.com>
2016-09-01 22:00:47 +0800

    [204. Count Primes][Accepted] +1s

--- 204-Count-Primes/solution.py
+++ 204-Count-Primes/solution.py
@@ -6,9 +6,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n < 3:
-            return 0
-        lst = [0,0]+[1]*(n-2)
+        lst = [0,0]+[1]*(max(2,n)-2)
         for i in range(int(sqrt(n))+1):
             if lst[i] == 1:
                 k = 2*i
