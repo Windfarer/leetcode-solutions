04dd0f11ed9e6de1b1003142774e8c94c9022375
Eric Yang <Windfarer@users.noreply.github.com>
2016-10-12 10:54:37 -0500

    [225. Implement Stack using Queues][Accepted] +1s

---
+++ 225-Implement-Stack-using-Queues/solution.py
@@ -0,0 +1,32 @@
+class Stack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        return self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not self.stack
+        
