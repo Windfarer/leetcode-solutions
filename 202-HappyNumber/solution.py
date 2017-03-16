59d1e6e41197d186966e9731233b9df694812ca8
Eric Yang <Windfarer@users.noreply.github.com>
2017-03-16 23:29:57 +0800

    happy ^_^

---
+++ 202-HappyNumber/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        s = set()
+        while n != 1:
+            n = sum([int(i)**2 for i in str(n)])
+            if n in s:
+                return False
+            s.add(n)
+        return True
