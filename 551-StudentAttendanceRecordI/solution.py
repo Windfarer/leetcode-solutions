243cf07b9b593c811574fad94224c97db7856161
Eric Yang <windfarer@gmail.com>
2017-07-08 14:24:48 +0800

    Create solution.py

---
+++ 551-StudentAttendanceRecordI/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        a_count = 0
+        l_count = 0
+        for i in s:
+            if i == 'L':
+                l_count += 1
+            if l_count > 2:
+                return False
+            if i == 'A':
+                a_count += 1
+            if i != 'L':
+                l_count = 0
+        if a_count > 1:
+            return False
+        return True
