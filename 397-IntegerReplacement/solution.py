cbbd1306c8be6b792f767446181b3f156c47908c
Eric Yang <windfarer@gmail.com>
2018-03-04 17:04:26 +0800

    Create solution.py

---
+++ 397-IntegerReplacement/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def integerReplacement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 0
+        elif n % 2 == 0:
+            return 1 + self.integerReplacement(n//2)
+        return 1 + min(self.integerReplacement(n+1), self.integerReplacement(n-1))
